9c77c9bb7cd4dec9e18d5f07e2227d73ae5d72aa
Aadiop1232 <khanaadi2030@gmail.com>
2025-03-08 05:17:13 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+# config.py
+TOKEN = "YOUR_TELEGRAM_BOT_TOKEN"
+OWNER_ID = 123456789  # Replace with your Telegram numeric owner ID
+BOT_USERNAME = "YourBotName"  # Without '@'
+REQUIRED_CHANNELS = [
+    "https://t.me/shadowsquad0",
+    "https://t.me/Originlabs",
+    "https://t.me/ShadowsquadHits",
+    "https://t.me/Binhub_Originlabs"
+]
+
