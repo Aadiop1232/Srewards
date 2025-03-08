324958f2555cc461601943954c3ae7c41df2c32d
Aadiop1232 <khanaadi2030@gmail.com>
2025-03-08 12:15:15 +0530

    Update main_menu.py

--- handlers/main_menu.py
+++ handlers/main_menu.py
@@ -7,8 +7,7 @@ def send_main_menu(bot, message):
     btn_rewards = types.InlineKeyboardButton("💳 Rewards", callback_data="menu_rewards")
     btn_account = types.InlineKeyboardButton("👤 Account Info", callback_data="menu_account")
     btn_referral = types.InlineKeyboardButton("🔗 Referral System", callback_data="menu_referral")
-    btn_review = types.InlineKeyboardButton("💬 Review/Suggestion", callback_data="menu_review")
+    btn_review = types.InlineKeyboardButton("💬 Review", callback_data="menu_review")
     btn_admin = types.InlineKeyboardButton("🛠 Admin Panel", callback_data="menu_admin")
     markup.add(btn_rewards, btn_account, btn_referral, btn_review, btn_admin)
     bot.send_message(message.chat.id, "📋 *Main Menu* 📋\nPlease choose an option:", parse_mode="Markdown", reply_markup=markup)
-    
