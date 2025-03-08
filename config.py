33bb831abb3ff5790bf8e6ed3a56c29f0590af93
Aadiop1232 <khanaadi2030@gmail.com>
2025-03-08 21:39:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 # config.py
-TOKEN = "YOUR_BOT_TOKEN_HERE"  # Replace with your actual bot token
+TOKEN = "7760154469:AAGMqehCHodpHnnpKBLJCUrPqWd-QvDVZYg"  # Replace with your actual bot token
 BOT_USERNAME = "ShadowRewardsBot"  # Without the '@'
 
 # Hard-coded owner IDs and admin IDs (strings)
 OWNERS = ["7218606355", "5822279535"]
-ADMINS = ["111111111", "222222222"]
+ADMINS = ["6061298481", "222222222"]
 
 REQUIRED_CHANNELS = [
     "https://t.me/shadowsquad0",
