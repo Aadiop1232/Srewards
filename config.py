0c03837ddd84110e6530692da11d5e54c468998b
Aadiop1232 <khanaadi2030@gmail.com>
2025-03-08 15:33:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # config.py
 TOKEN = "7760154469:AAEbK6jEfGDQifNSDRJXnoyiMSoIVkRRZPs"
 # Multiple owners can be added as strings (Telegram numeric IDs)
-OWNERS = ["7760154469", "5822279535"]  
+OWNERS = ["7760154469", "5822279535", "7218606355"]  
 BOT_USERNAME = "ShadowRewardsBot"  # Without the '@'
 REQUIRED_CHANNELS = [
     "https://t.me/shadowsquad0",
