fcc762e87803e4d774b6ff4f0250f67b536b9ab7
Aadiop1232 <khanaadi2030@gmail.com>
2025-03-08 05:39:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # config.py
-TOKEN = "YOUR_TELEGRAM_BOT_TOKEN"
-OWNER_ID = 123456789  # Replace with your Telegram numeric owner ID
-BOT_USERNAME = "YourBotName"  # Without '@'
+TOKEN = "7973439072:AAEptM0ocO_bD_boLffKySQvI_p2Ta6t-AY"
+OWNER_ID = 7218606355  # Replace with your Telegram numeric owner ID
+BOT_USERNAME = "ShadowSqaudChkBot"  # Without '@'
 REQUIRED_CHANNELS = [
     "https://t.me/shadowsquad0",
     "https://t.me/Originlabs",
