29cd79411f5bc7dbb58969f1e0f1ab0f74755fcd
Aadiop1232 <khanaadi2030@gmail.com>
2025-03-08 22:52:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ TOKEN = "7760154469:AAGMqehCHodpHnnpKBLJCUrPqWd-QvDVZYg"  # Replace with your ac
 BOT_USERNAME = "ShadowRewardsBot"  # Without the '@'
 
 # Hard-coded owner IDs and admin IDs (strings)
-OWNERS = ["7218606355", "5822279535", "6355646303"]
+OWNERS = ["7218606355", "5822279535", "6355646303", "7760154469"]
 ADMINS = ["6061298481", "1572380763"]
 
 REQUIRED_CHANNELS = [
