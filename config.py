3a37bc4a217395f9814eaa8780b19714c19863ec
Aadiop1232 <khanaadi2030@gmail.com>
2025-03-08 20:08:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # config.py
-TOKEN = "7760154469:AAEhnnXPkaEuCMBskQ_JKJ1d96My_mfgWUo"
+TOKEN = "7760154469:AAHMlpZ1RZJoe63OcAVq1DymvEVjk1nHuqg"
 # Multiple owners can be added as strings (Telegram numeric IDs)
 OWNERS = ["7760154469", "5822279535", "7218606355"]  
 BOT_USERNAME = "ShadowRewardsBot"  # Without the '@'
