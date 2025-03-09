4dd1c2785cdc97de2741b0f4da46c9c24dcf94c6
Aadiop1232 <khanaadi2030@gmail.com>
2025-03-09 08:57:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ TOKEN = "7760154469:AAFMteXwMHZU27J0aCqlUjqgaheZaJf6T9w"
 BOT_USERNAME = "ShadowRewardsBot"
 # You can mix numeric IDs and usernames (usernames without '@')
 OWNERS = ["7218606355", "MrLazyOp"]
-ADMINS = ["7218606355", "yourusername", "38461874"]
+ADMINS = ["7218606355", "MrLazyOp"]
 REQUIRED_CHANNELS = [
     "https://t.me/shadowsquad0",
     "https://t.me/Originlabs",
