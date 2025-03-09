ab508a10c2780ebe7c766ca7e3b7c440e47af7ad
Aadiop1232 <khanaadi2030@gmail.com>
2025-03-09 09:25:47 +0530

    Update main_menu.py

--- handlers/main_menu.py
+++ handlers/main_menu.py
@@ -1,14 +1,18 @@
 # handlers/main_menu.py
 import telebot
 from telebot import types
+from handlers.admin import is_admin
 
 def send_main_menu(bot, message):
+    user_obj = message.from_user
     markup = types.InlineKeyboardMarkup(row_width=3)
     btn_rewards = types.InlineKeyboardButton("💳 Rewards", callback_data="menu_rewards")
     btn_account = types.InlineKeyboardButton("👤 Account Info", callback_data="menu_account")
     btn_referral = types.InlineKeyboardButton("🔗 Referral System", callback_data="menu_referral")
     btn_review = types.InlineKeyboardButton("💬 Review", callback_data="menu_review")
-    btn_admin = types.InlineKeyboardButton("🛠 Admin Panel", callback_data="menu_admin")
-    markup.add(btn_rewards, btn_account, btn_referral, btn_review, btn_admin)
+    markup.add(btn_rewards, btn_account, btn_referral, btn_review)
+    if is_admin(user_obj):
+        btn_admin = types.InlineKeyboardButton("🛠 Admin Panel", callback_data="menu_admin")
+        markup.add(btn_admin)
     bot.send_message(message.chat.id, "<b>📋 Main Menu 📋</b>\nPlease choose an option:", parse_mode="HTML", reply_markup=markup)
     
