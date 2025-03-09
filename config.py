eaa525809ecb9d47b25db221b008bddcc1fa4011
Aadiop1232 <khanaadi2030@gmail.com>
2025-03-09 06:19:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # config.py
-TOKEN = "YOUR_BOT_TOKEN_HERE"  # Replace with your actual bot token
+TOKEN = "7760154469:AAHXLYuSgHHWpP9TVUm-bPiy0wBI0U_wHTg"  # Replace with your actual bot token
 BOT_USERNAME = "ShadowRewardsBot"  # Without the '@'
 
 # Hard-coded internal IDs for owners and admins.
