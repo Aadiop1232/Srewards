5fce1cbcf941987ccf25fe0f2374c570bac9cc90
Aadiop1232 <khanaadi2030@gmail.com>
2025-03-09 08:19:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,8 @@
 # config.py
-TOKEN = "7760154469:AAHXLYuSgHHWpP9TVUm-bPiy0wBI0U_wHTg"  # Replace with your actual bot token
-BOT_USERNAME = "ShadowRewardsBot"  # Without the '@'
-
-# Hard-coded internal IDs for owners and admins.
-# After a user registers, check their internal_id from the database and update these lists accordingly.
+TOKEN = "7760154469:AAHXLYuSgHHWpP9TVUm-bPiy0wBI0U_wHTg"
+BOT_USERNAME = "ShadowRewardsBot"
 OWNERS = ["7218606355", "38461874"]
 ADMINS = ["7218606355", "38461874"]
-
 REQUIRED_CHANNELS = [
     "https://t.me/shadowsquad0",
     "https://t.me/Originlabs",
