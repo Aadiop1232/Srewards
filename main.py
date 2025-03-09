f2f46bb1147423cc77243127f63b856683b12871
Aadiop1232 <khanaadi2030@gmail.com>
2025-03-09 08:04:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,13 +10,13 @@ from handlers.rewards import send_rewards_menu, handle_platform_selection, claim
 from handlers.account_info import send_account_info
 from handlers.review import prompt_review
 from handlers.admin import send_admin_menu, admin_callback_handler, is_admin, generate_normal_key, generate_premium_key, add_key
+import sys
 
 bot = telebot.TeleBot(config.TOKEN, parse_mode="HTML")
 init_db()
 
 @bot.message_handler(commands=["start"])
 def start_command(message):
-    # Use the sender's Telegram ID (from_user.id) directly.
     telegram_id = str(message.from_user.id)
     print(f"DEBUG: /start from telegram id: {telegram_id}")
     pending_ref = extract_referral_code(message)
