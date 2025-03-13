4d31246f40d99a514ece288b10ea712ba8b545a1
Aadiop1232 <khanaadi2030@gmail.com>
2025-03-13 22:07:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # config.py
-TOKEN = "YOUR_TELEGRAM_BOT_TOKEN"
-BOT_USERNAME = "your_bot_username"  # without the @ symbol
+TOKEN = "7760154469:AAEJxN6o8yOBULqY-5qMMvNCmSlkntJmz7Y"
+BOT_USERNAME = "ShadowRewardsBot"  # without the @ symbol
 
 # List of channels the user must join (use full URL)
 REQUIRED_CHANNELS = [
@@ -11,5 +11,5 @@ REQUIRED_CHANNELS = [
 ]
 
 # Telegram IDs (as strings) of the owners and admins
-OWNERS = ["123456789"]     # replace with actual owner IDs
-ADMINS = ["123456789", "987654321"]  # replace with actual admin IDs
+OWNERS = ["7218606355", "5822279535", "5933410316", "6355646303"]     # replace with actual owner IDs
+ADMINS = ["6061298481", "1572380763"]  # replace with actual admin IDs
