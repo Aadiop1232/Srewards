c4305abd4e9ca5b12458979a0bf89caf189f9342
Aadiop1232 <khanaadi2030@gmail.com>
2025-03-14 22:46:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,7 @@
-# config.py
 TOKEN = "7760154469:AAEJxN6o8yOBULqY-5qMMvNCmSlkntJmz7Y"
-BOT_USERNAME = "ShadowRewardsBot"  # without the @ symbol
+BOT_USERNAME = "ShadowRewardsBot"
 
-# List of channels the user must join (use full URL)
+
 REQUIRED_CHANNELS = [
     "https://t.me/shadowsquad0",
     "https://t.me/Originlabs",
@@ -10,6 +9,5 @@ REQUIRED_CHANNELS = [
     "https://t.me/Binhub_Originlabs"
 ]
 
-# Telegram IDs (as strings) of the owners and admins
-OWNERS = ["7218606355", "5822279535", "5933410316", "6355646303"]     # replace with actual owner IDs
-ADMINS = ["6061298481", "1572380763"]  # replace with actual admin IDs
+OWNERS = ["7218606355", "5822279535", "5933410316", "6355646303"]
+ADMINS = ["6061298481", "1572380763"]
