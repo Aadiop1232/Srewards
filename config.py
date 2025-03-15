cd873b29272df3ebd9ddc0a07771f895291b3fa1
Aadiop1232 <khanaadi2030@gmail.com>
2025-03-16 02:03:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@
 TOKEN = "7760154469:AAFwBrol9EP2L78Wt0sTqXcoz3OiL4HPy8I"
 BOT_USERNAME = "ShadowRewardsBot"  # without the '@' symbol
 
-# Required channels users must join
+# Required channels that users must join
 REQUIRED_CHANNELS = [
     "https://t.me/shadowsquad0",
     "https://t.me/Originlabs",
@@ -16,12 +16,8 @@ REQUIRED_CHANNELS = [
 OWNERS = ["7218606355", "5822279535", "5933410316", "6355646303"]
 ADMINS = ["6061298481", "1572380763"]
 
-# Logs channel where bot events will be posted
+# Logs channel (bot must be added there with full privileges)
 LOGS_CHANNEL = "@ShadowBotLogs"
-
-# MongoDB connection settings
-MONGO_URI = "mongodb+srv://khanaadi2030:sRBArJUApUR53jSM@cluster0.oy1in.mongodb.net/shadow_rewards_db?retryWrites=true&w=majority"
-MONGO_DB_NAME = "shadow_rewards_db"
 
 # Default dynamic configuration values
 DEFAULT_ACCOUNT_CLAIM_COST = 2   # Points required to claim an account
