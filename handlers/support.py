33d8ac1e05268c2b7dff9822606af6b341b86cb1
Aadiop1232 <khanaadi2030@gmail.com>
2025-03-18 09:29:25 +0530

    Update support.py

--- handlers/support.py
+++ handlers/support.py
@@ -1,3 +1,3 @@
 def send_support_message(bot, message):
-    text = "For support, contact @YourSupportUsername or join our support group at https://t.me/YourSupportGroup."
+    text = "For support, contact @MrLazyOp or join our support group at https://t.me/ShadowBotSupportChat ."
     bot.send_message(message.chat.id, text)
