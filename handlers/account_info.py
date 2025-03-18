7bcb1051b50b3d525612ca36faf60db17473c567
Aadiop1232 <khanaadi2030@gmail.com>
2025-03-18 16:04:49 +0530

    Update account_info.py

--- handlers/account_info.py
+++ handlers/account_info.py
@@ -3,20 +3,16 @@ from datetime import datetime
 
 def send_account_info(bot, update):
     if hasattr(update, "data"):
-        # update is a callback query; use update.from_user
         chat_id = update.message.chat.id
         user_obj = update.from_user
     else:
-        # update is a message
-        chat_id = update.message.chat.id
-        user_obj = update.message.from_user
-
+        chat_id = update.chat.id
+        user_obj = update.from_user
     telegram_id = str(user_obj.id)
     user = get_user(telegram_id)
     if not user:
         add_user(telegram_id, user_obj.username or user_obj.first_name, datetime.now().strftime("%Y-%m-%d"))
         user = get_user(telegram_id)
-    
     text = (
         "<b>👤 Account Info</b>\n"
         f"• <b>Username:</b> {user.get('username')}\n"
