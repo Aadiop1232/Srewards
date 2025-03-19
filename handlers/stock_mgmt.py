796f3c43d79cd689e012849d8a877b67b51829c8
Aadiop1232 <khanaadi2030@gmail.com>
2025-03-19 23:12:00 +0530

    Update stock_mgmt.py

--- handlers/stock_mgmt.py
+++ handlers/stock_mgmt.py
@@ -122,5 +122,4 @@ def process_cookie_file(message, bot, platform_name):
     from handlers.admin import send_admin_menu
     send_admin_menu(bot, message)
 
-bot.answer_callback_query(call.id, text="OK", show_alert=False)
 
