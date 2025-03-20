762ea435c4b581ba09958d04993d5b07a72a48f3
Aadiop1232 <khanaadi2030@gmail.com>
2025-03-21 01:30:04 +0530

    Delete handlers/support.py

--- handlers/support.py
+++
@@ -1,3 +0,0 @@
-def send_support_message(bot, message):
-    text = "For support, contact @MrLazyOp or join our support group at https://t.me/ShadowBotSupportChat ."
-    bot.send_message(message.chat.id, text)
